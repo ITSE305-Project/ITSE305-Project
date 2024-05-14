b18cc904982c58b37056db22b43350072e8b9424
ITSE305-Project <163577479+ITSE305-Project@users.noreply.github.com>
2024-05-14 20:13:03 +0300

    Shakeel updated these files after review

--- AccountTest_Transfer.java
+++ AccountTest_Transfer.java
@@ -5,19 +5,19 @@ class AccountTest_Transfer {
 
     @Test
     void testGetIBAN() {
-        Account account = new Account("123456789", 1000.0);
+        TransferAccount account = new TransferAccount("123456789", 1000.0);
         assertEquals("123456789", account.getIBAN());
     }
 
     @Test
     void testGetBalance() {
-        Account account = new Account("123456789", 1000.0);
+        TransferAccount account = new TransferAccount("123456789", 1000.0);
         assertEquals(1000.0, account.getBalance());
     }
 
     @Test
     void testSetBalance() {
-        Account account = new Account("123456789", 1000.0);
+        TransferAccount account = new TransferAccount("123456789", 1000.0);
         account.setBalance(1500.0);
         assertEquals(1500.0, account.getBalance());
     }
